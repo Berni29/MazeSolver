1f1bbe4b98c7d2545070a243ee744926ffe7b433
Berni29 <boczarskitomasz@gmail.com>
2018-03-23 13:23:59 +0100

    Changes in wallfollower and threads

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,48 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     private JPanel form;
-    private JButton GOButton;
-    private JButton LOADMAZEButton;
+    private JButton wallFollowerRightButton;
+    private JButton loadMazeButton;
     private JLabel mazeArea;
+    private JButton wallFollowerLeftButton;
+    private JButton resetButton;
+    private JSlider speed;
     private MazeFromFile mff = new MazeFromFile();
     private WallFollower wf = null;
     private BufferedImage img;
     private Graphics2D walker;
+    private File file;
+    private boolean kill = false;
 
     public Main() {
-        GOButton.addActionListener(new ActionListener() {
+        wallFollowerRightButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 if(wf==null){
                     JOptionPane.showMessageDialog(form,"Maze not loaded");
                 }
                 else {
-                    Executor executor = Executors.newSingleThreadExecutor();
-                    executor.execute(new Runnable() {
+                    reset();
+                    Thread thread = new Thread(new Runnable() {
                         @Override
                         public void run() {
-                            if(wf!=null) {
-                                ImageIcon icon = new ImageIcon(img);
-                                while (!wf.destinationReached()) {
-                                    try {
-                                        wf.move(walker);
-                                        mazeArea.setIcon(icon);
-                                        TimeUnit.MILLISECONDS.sleep(1);
-                                        mazeArea.repaint();
-                                    }
-                                    catch (InterruptedException e) {
-                                        System.err.println(e.getMessage());
-                                    }
-                                }
-                            }
+                            walk("Right");
                         }
                     });
+                    thread.start();
                 }
             }
         });
-        LOADMAZEButton.addActionListener(new ActionListener() {
+        loadMazeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                     JFileChooser fc = new JFileChooser();
@@ -60,7 +50,8 @@ public class Main {
                     fc.setFileFilter(filter);
                     int result = fc.showOpenDialog(form);
                     if(result == JFileChooser.APPROVE_OPTION) {
-                        mff.createMaze(fc.getSelectedFile());
+                        file = fc.getSelectedFile();
+                        mff.createMaze(file);
                         img = mff.getImageMaze();
                         walker = (Graphics2D) img.getGraphics();
                         wf = null;
@@ -70,6 +61,30 @@ public class Main {
             }
 
         });
+        wallFollowerLeftButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(wf==null){
+                    JOptionPane.showMessageDialog(form,"Maze not loaded");
+                }
+                else {
+                    reset();
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            walk("Left");
+                        }
+                    });
+                    thread.start();
+                }
+            }
+        });
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                reset();
+            }
+        });
     }
 
     public static void main(String[] args) {
@@ -78,6 +93,47 @@ public class Main {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public void walk(String direction){
+        if(direction.equals("Right")) {
+            ImageIcon icon = new ImageIcon(img);
+            while (!kill && !wf.destinationReached()) {
+                try {
+                    wf.moveRight(walker);
+                    mazeArea.setIcon(icon);
+                    TimeUnit.MILLISECONDS.sleep(101-speed.getValue());
+                    mazeArea.repaint();
+                } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+        else {
+            ImageIcon icon = new ImageIcon(img);
+            while (!kill && !wf.destinationReached()) {
+                try {
+                    wf.moveLeft(walker);
+                    mazeArea.setIcon(icon);
+                    TimeUnit.MILLISECONDS.sleep(101-speed.getValue());
+                    mazeArea.repaint();
+                } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+                }
+            }
+        }
+    }
+    public void reset(){
+        kill = true;
+        if(file!=null){
+            mff.clearMazeTrace();
+            img = mff.getImageMaze();
+            walker = (Graphics2D) img.getGraphics();
+            wf = null;
+            wf = new WallFollower(mff.getMaze());
+            mazeArea.setIcon(new ImageIcon(img.getScaledInstance(img.getWidth(), img.getHeight(), Image.SCALE_DEFAULT)));
+        }
+        kill = false;
     }
 
 }
--- src/WallFollower.java
+++ src/WallFollower.java
@@ -13,7 +13,48 @@ public class WallFollower {
         this.maze[0][0].setVisited(true);
     }
 
-    public void move(Graphics2D g) {
+    public void moveRight(Graphics2D g) {
+        g.setColor(Color.GREEN);
+        if(!maze[posX][posY].isRight() && !maze[posX+1][posY].isVisited()) {
+            g.drawLine((posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2),(posX*CELLSIZE)+(3*CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2));
+            posX++;
+        }
+        else if(!maze[posX][posY].isBottom() && !maze[posX][posY+1].isVisited()) {
+            g.drawLine((posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2),(posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(3*CELLSIZE/2));
+            posY++;
+        }
+        else if(!maze[posX][posY].isLeft() && !maze[posX-1][posY].isVisited()) {
+            g.drawLine((posX*CELLSIZE)-(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2),(posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2));
+            posX--;
+        }
+        else if (!maze[posX][posY].isTop() && !maze[posX][posY-1].isVisited()) {
+            g.drawLine((posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)-(CELLSIZE/2),(posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2));
+            posY--;
+        }
+        else {
+            maze[posX][posY].setRevisited(true);
+            g.setColor(Color.BLUE);
+            if (!maze[posX][posY].isRight() && !maze[posX+1][posY].isRevisited()) {
+                g.drawLine((posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2),(posX*CELLSIZE)+(3*CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2));
+                posX++;
+            }
+            else if (!maze[posX][posY].isBottom() && !maze[posX][posY+1].isRevisited()) {
+                g.drawLine((posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2),(posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(3*CELLSIZE/2));
+                posY++;
+            }
+            else if(!maze[posX][posY].isLeft() && !maze[posX-1][posY].isRevisited()) {
+                g.drawLine((posX*CELLSIZE)-(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2),(posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2));
+                posX--;
+            }
+            else if (!maze[posX][posY].isTop() && !maze[posX][posY-1].isRevisited()) {
+                g.drawLine((posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)-(CELLSIZE/2),(posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2));
+                posY--;
+            }
+        }
+        maze[posX][posY].setVisited(true);
+    }
+
+    public void moveLeft(Graphics2D g) {
         g.setColor(Color.GREEN);
         if(!maze[posX][posY].isLeft() && !maze[posX-1][posY].isVisited()) {
             g.drawLine((posX*CELLSIZE)-(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2),(posX*CELLSIZE)+(CELLSIZE/2),(posY*CELLSIZE)+(CELLSIZE/2));
